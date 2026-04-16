eec2e04f0f662a0835fd30c99b3323a48c03cd1c
ChinaZZH <565936130@qq.com>
2026-04-16 15:52:54 +0800

    Update README.md
    通过mutex互斥锁实现一个生产者消费者代码。

---
+++ day1-producer-consumer/producer_consumer_queue.cpp
@@ -0,0 +1,75 @@
+﻿#include <thread>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <chrono>
+
+
+std::queue<int> qTest;
+std::mutex mtx;
+const int MAX_QUEUE_COUNT = 5;
+
+void proceducer()
+{
+	for(int i = 0; i < 10; )
+	{
+		std::this_thread::sleep_for(std::chrono::microseconds(100));
+		if(qTest.size() < MAX_QUEUE_COUNT)
+		{
+			{
+				std::lock_guard<std::mutex> lk(mtx);
+				qTest.push(i);
+				
+			}
+			
+			std::string str("Proceduced=");
+			str.append(std::to_string(i));
+			str.append("\r\n");
+			std::cout << str.c_str();
+
+			++i;
+		}
+		else {
+			std::this_thread::sleep_for(std::chrono::microseconds(10));
+		}
+	}
+}
+
+
+void consumer()
+{
+	for (int i = 0; i < 10; )
+	{
+		std::this_thread::sleep_for(std::chrono::microseconds(100));
+		if(!qTest.empty())
+		{
+			int nGetValue = 0;
+			{
+				std::lock_guard<std::mutex> lk(mtx);
+				nGetValue = qTest.front();
+				qTest.pop();
+				//std::cout << "Consumed=" << nGetValue << std::endl;
+			}
+			
+			std::string str("Consumed=");
+			str.append(std::to_string(nGetValue));
+			str.append("\r\n");
+			std::cout << str.c_str();
+
+			++i;
+		}
+		else {
+			std::this_thread::sleep_for(std::chrono::microseconds(10));
+		}
+	}
+}
+
+
+int main()
+{
+	std::thread t1(proceducer);
+	std::thread t2(consumer);
+	t1.join();
+	t2.join();
+	return 0;
+}
