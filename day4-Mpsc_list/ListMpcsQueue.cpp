6f12995e3247b02e8acc89e446fb7da0ccf50911
ChinaZZH <565936130@qq.com>
2026-04-19 09:44:11 +0800

    基于链表的无锁队列（MPSC）
    实现一个基于链表的 MPSC 无锁队列。核心是理解多个生产者如何通过 exchange 原子地竞争 tail，消费者如何安全地取数据。无锁队列是实现高并发消息传递的基础。

---
+++ day4-Mpsc_list/ListMpcsQueue.cpp
@@ -0,0 +1,111 @@
+#include <thread>
+#include <iostream>
+#include <atomic>
+#include <vector>
+#include <chrono>
+#include <mutex>
+
+
+template<typename T>
+
+// 基于cas的mpcs队列
+class ListMpscQueue
+{
+private:
+	struct Node {
+		T data;
+		std::atomic<Node*> next;
+		
+		Node(const T& value) : data(value), next(nullptr) {} 
+	};
+
+	std::atomic<Node*> head;
+	std::atomic<Node*> tail;
+
+public:
+	ListMpscQueue()
+	{
+		// 默认有头结点，避免head和tail节点为nullptr,就是head,tail一定有数值,
+		// 则避免了判断head和tail是否为空
+		Node* dummy = new Node(T());
+		head.store(dummy, std::memory_order_relaxed);
+		tail.store(dummy, std::memory_order_relaxed);
+	}
+
+	~ListMpscQueue()
+	{
+
+	}
+
+	// 多线程安全
+	void push(const T& value)
+	{
+		Node* new_node = new Node(value);
+		Node* old_tail = tail.exchange(new_node, std::memory_order_acq_rel);
+		old_tail->next.store(tail, std::memory_order_release);
+	}
+
+	// 单线程执行版本
+	bool pop(T& value)
+	{
+		Node* old_head_node = head.load(std::memory_order_acquire);
+		Node* head_next = old_head_node->next.load(std::memory_order_acquire);
+		if(nullptr == head_next)
+		{
+			return false;
+		}
+
+		value = head_next->data;
+		head.store(head_next, std::memory_order_release);
+		delete old_head_node;
+		return true;
+	}
+};
+
+
+int main()
+{
+	ListMpscQueue<int> arrayQueue;
+	const int MaxNum = 10;
+
+	auto producer = [&](int nStart) {
+		for (int i = 0; i < MaxNum; ++i)
+		{
+			int value = i + nStart;
+			arrayQueue.push(value);
+		}
+		};
+
+
+	int nSum = 0;
+	auto consume = [&]() {
+		int nValue = 0;
+		for (int index = 0; index < 3 * MaxNum; )
+		{
+			if (arrayQueue.pop(nValue))
+			{
+				nSum += nValue;
+				++index;
+			}
+		}
+		};
+
+
+	std::thread pro_t1(producer, 0);
+	std::thread pro_t2(producer, MaxNum);
+	std::thread pro_t3(producer, MaxNum + MaxNum);
+
+	std::thread consume_t(consume);
+
+	pro_t1.join();
+	pro_t2.join();
+	pro_t3.join();
+	consume_t.join();
+
+	std::cout << "Sum:" << nSum << std::endl;
+	return 0;
+}
+
+
+
+
