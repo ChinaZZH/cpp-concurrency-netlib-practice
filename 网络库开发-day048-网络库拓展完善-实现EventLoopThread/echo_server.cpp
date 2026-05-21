d918520d4a8b329db7c5104935b048d2031e7b2d
ChinaZZH <565936130@qq.com>
2026-05-21 16:40:45 +0800

    版本初始化(迁移上一个版本)-day48-实现EventLoopThread

---
+++ 网络库开发-day048-网络库拓展完善-实现EventLoopThread/echo_server.cpp
@@ -0,0 +1,39 @@
+#include "EventLoop.h"
+#include "TcpServer.h"
+#include "HttpServer.h"
+#include <signal.h>
+
+#define PORT 8888
+
+
+int main()
+{
+    signal(SIGPIPE, SIG_IGN);
+    
+    EventLoop loop;
+    TcpServer server(&loop, PORT);
+    server.SetMessageCallBack(std::bind(&TcpServer::HandleOnMessage, 
+        &server, std::placeholders::_1, 
+        std::placeholders::_2)
+    );
+    server.Start(0);
+
+    // httpSrver
+    //HttpServer server(&loop, PORT);
+    //server.Start(0);
+
+    // 定时器测试
+    /*
+    int num = 1;
+    loop.RunEvery(std::chrono::seconds(1), [&num](){
+        std::cout << "Tick = " << num << std::endl;
+        ++num;
+    });
+    */
+
+    loop.Loop();
+    return 0;
+}
+
+
+
