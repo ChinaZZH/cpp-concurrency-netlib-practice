e86848f8089dbbcf8499e82817ec010b40904fb8
ChinaZZH <565936130@qq.com>
2026-05-22 18:20:04 +0800

    版本初始化(从上个版本迁移)-day49-实现EventLoopThreadPool

---
+++ 网络库开发-day049-网络库拓展完善-实现EventLoopThreadPool/TcpServer.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+#include <map>
+#include <unordered_map>
+#include <thread>
+#include "ThreadPool.h"
+
+
+class EventLoop;
+class ListenSocket;
+class Channel;
+class TcpConnection;
+
+
+class TcpServer
+{
+public:
+    using MessageCallBack = std::function<void(const std::shared_ptr<TcpConnection>&, std::string&)>;
+    using CloseCallBack = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+
+    TcpServer(EventLoop* loop, int nPort);
+    
+    ~TcpServer();
+
+    void Start(int option, int nThreadNum = std::thread::hardware_concurrency() - 1);
+
+    void SetMessageCallBack(MessageCallBack cb) { messageCallBack_ = cb; }
+    void SetCloseCallBack(CloseCallBack cb) { closeCallBack_ = cb; }
+
+    ThreadPool* GetThreadPool() { return threadPool_.get(); }
+    EventLoop* GetEventLoop()   { return loop_; }
+    std::shared_ptr<TcpConnection> GetTcpConnection(int fd);
+
+    void HandleOnMessage(const std::shared_ptr<TcpConnection>& con, std::string& strMsg);
+    void RemoveConnectionByFd(int fd);
+
+    // 设置空闲时间
+    void SetConnectionIdleTimeOut(int nSecs);
+private:
+    void HandleNewConnection();
+    void ClosedConnection(const std::shared_ptr<TcpConnection>& conn);
+
+    void CheckIdleConnections();
+
+private:
+    EventLoop* loop_;
+    int port_;
+    std::unique_ptr<ListenSocket> listenSocket_;
+    std::map<int, std::shared_ptr<TcpConnection>> mapTcpConnection_;
+
+    MessageCallBack messageCallBack_;
+    CloseCallBack closeCallBack_;
+    std::unique_ptr<ThreadPool> threadPool_;
+
+    int idleTimeOutSecs_ = 60; // 连接空闲时间断开，默认是60秒(<=0 则空闲时间可以无限)
+};
