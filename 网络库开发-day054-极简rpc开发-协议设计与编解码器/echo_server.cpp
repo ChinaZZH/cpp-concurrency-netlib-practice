ef02feae60112e067dbf4c75874936b0be6dc6a0
ChinaZZH <565936130@qq.com>
2026-05-26 10:35:15 +0800

    版本初始化(迁移上一个版本)-day054-协议设计和编码解码器的开发设计

---
+++ 网络库开发-day054-极简rpc开发-协议设计与编解码器/echo_server.cpp
@@ -0,0 +1,39 @@
+#include "EventLoopThread.h"
+#include "EventLoopThreadPool.h"
+#include "EventLoop.h"
+#include "TcpServer.h"
+#include "HttpServer.h"
+#include <iostream>
+#include <chrono>
+#include <iostream>
+
+#include <signal.h>
+#include <thread>
+
+#define PORT 8888
+
+
+int main()
+{
+    
+    signal(SIGPIPE, SIG_IGN);
+    
+    EventLoop loop;
+    
+    TcpServer server(&loop, PORT);
+    server.SetMessageCallBack(std::bind(&TcpServer::HandleOnMessage, 
+        &server, std::placeholders::_1, 
+        std::placeholders::_2)
+    );
+    server.Start(0, 6, 5);
+    
+
+    // httpSrver
+    //HttpServer server(&loop, PORT);
+    //server.Start(0, 6, 60); // eventloopThread 6个工作线程为最佳性能
+    loop.Loop();
+    return 0;
+}
+
+
+
