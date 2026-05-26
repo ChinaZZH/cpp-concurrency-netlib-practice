2221fb18aa04ae41aa1445a52ffb8ad4fc82173c
ChinaZZH <565936130@qq.com>
2026-05-26 13:42:07 +0800

    版本初始化(上一个版本迁移)-day55-搭建rpcServer骨架

---
+++ 网络库开发-day055-极简rpc开发-搭建rpcServer骨架/LibRpc/RpcCodec.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include "../Buffer.h"
+
+class RpcCodec
+{
+public:
+    // 编码请求
+    static void EncodeRequest(Buffer& buffer, uint32_t id, const std::string& method, const std::string& params);
+
+    // 解码请求， 成功则返回true 并且填充 id, method， params; 失败则返回false (数据不足或者格式错误)
+    static bool DecodeRequest(Buffer& buffer, uint32_t& id, std::string& method, std::string& params);
+
+    // 编码响应
+    static void EncodeResponse(Buffer& buffer, uint32_t id, int32_t code, const std::string& result);
+
+    // 解码响应  成功返回true; 失败则返回false
+    static bool DecodeResponse(Buffer& buffer, uint32_t& id, int32_t& code, std::string& result);
+
+    // 四个辅助函数
+    static void WriteInt32(Buffer& buffer, int32_t value);
+
+    static bool ReadInt32(Buffer& buffer, int32_t& value);
+
+    static void WriteString(Buffer& buffer, const std::string& strValue);
+
+    static bool ReadString(Buffer& buffer, std::string& strValue);
+};
