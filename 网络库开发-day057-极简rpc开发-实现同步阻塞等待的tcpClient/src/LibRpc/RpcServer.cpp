c2c3b42e31532ee1f6841d0c2e34800aec6d9e68
ChinaZZH <565936130@qq.com>
2026-05-27 09:29:10 +0800

    版本初始化(迁移自上一个版本)-day057-实现同步阻塞等待的tcpClient

---
+++ 网络库开发-day057-极简rpc开发-实现同步阻塞等待的tcpClient/src/LibRpc/RpcServer.cpp
@@ -0,0 +1,78 @@
+
+#include "RpcServer.h"
+#include "RpcCodec.h"
+#include "../TcpConnection.h"
+#include "../EventLoop.h"
+
+
+RpcServer::RpcServer(EventLoop* loop, int nPort)
+:server_(loop, nPort)
+{
+    server_.SetMessageCallBack(std::bind(&RpcServer::OnMessage, 
+        this, std::placeholders::_1, 
+        std::placeholders::_2)
+    );
+}
+
+
+void RpcServer::Start(int option, int nEventLoopThread, int idleSecTimeOut, int nTaskThreadNum /*= std::thread::hardware_concurrency()*/)
+{
+    server_.Start(option, nEventLoopThread, idleSecTimeOut, nTaskThreadNum);
+}
+
+
+void RpcServer::RegisterMethod(const std::string& strMethod, Handler handler)
+{
+    methods_[strMethod] = handler;
+}
+
+
+void RpcServer::OnMessage(const std::shared_ptr<TcpConnection>& con, std::string& strMsg)
+{
+    con->GetLoop()->AssertInLoopThread("RpcServer::OnMessage");
+
+    Buffer buf;
+    buf.Append(strMsg.data(), strMsg.size());
+
+    uint32_t id;
+    std::string strMethod, strParams;
+    bool ok = RpcCodec::DecodeRequest(buf, id, strMethod, strParams);
+    if(!ok)
+    {
+        this->HandlerResultResponse(con, id, eRpcCode_ParamError, "{}");
+        return;
+    }
+
+    std::cout << "RpcServer::OnMessage id:="  << id << " method:"  << strMethod.c_str() << " params:" << strParams.c_str() << std::endl;
+    auto itr = methods_.find(strMethod);
+    if(itr == methods_.end())
+    {
+        this->HandlerResultResponse(con, id, eRpcCode_NotGetMethod, "{}");
+        return;
+    }
+
+    std::string result;
+    try{
+        Handler handler = (itr->second);
+        result = handler(strParams);
+    }
+    catch(const std::exception& e)
+    {
+        this->HandlerResultResponse(con, id, eRpcCode_ServerError, "{}");
+        return;
+    }
+
+    this->HandlerResultResponse(con, id, eRpcCode_Success, std::move(result));
+}
+
+
+void RpcServer::HandlerResultResponse(const std::shared_ptr<TcpConnection>& con, uint32_t id, int32_t code, std::string strResult)
+{
+    Buffer resBuf;
+    RpcCodec::EncodeResponse(resBuf, id, code, strResult);
+
+    std::string strResponse = resBuf.RetrieveAllAsString();
+    con->Send(strResponse);
+}
+
+       
