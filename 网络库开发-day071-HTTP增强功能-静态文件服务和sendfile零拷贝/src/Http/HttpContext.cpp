b44c84529773a46dd389cc44a33cab5fa077418c
ChinaZZH <565936130@qq.com>
2026-06-10 15:37:40 +0800

    版本初始化(迁移至上一个版本)-day071-静态文件服务和sendfile零拷贝

---
+++ 网络库开发-day071-HTTP增强功能-静态文件服务和sendfile零拷贝/src/Http/HttpContext.cpp
@@ -0,0 +1,275 @@
+#include "HttpContext.h"
+#include <iostream>
+#include <algorithm>
+
+
+HttpContext::HttpContext()
+:method_()
+,path_()
+,version_()
+,body_()
+,nullptr_header()
+,status_code_(0)
+,response_body_()
+{
+
+}
+
+
+const std::string& HttpContext::GetHeader(const std::string& key) const
+{
+    auto itr = std::find_if(headers_.begin(), headers_.end(), 
+                            [&key](const std::pair<std::string, std::string>& pairHead){ 
+                                return pairHead.first.compare(key);
+                        });
+
+
+    if(itr == headers_.end())
+    {
+        return nullptr_header;
+    }
+
+    return (itr->second);
+}
+
+
+// 重置状态，用于复用对象(例如 keep_alive)
+void HttpContext::Reset()
+{
+    method_.clear();
+    path_.clear();
+    version_.clear();
+    headers_.clear();
+    body_.clear();
+    nullptr_header.clear();
+
+    status_code_ = 0;
+    response_body_.clear();
+}
+
+
+/*
+Get的请求报文格式
+GET /index.html?name=john&age=25 HTTP/1.1\r\n
+Host: www.example.com\r\n
+User-Agent: Mozilla/5.0\r\n
+Accept: text/html\r\n
+\r\n
+*/
+
+// 解析数据， 返回true表示解析数据完成(可能还有剩余数据未使用)
+bool HttpContext::ParseRequest(const std::string& data)
+{
+     enum ParseRequestState{
+        kExpectRequestLine,
+        kExpectHeaders,
+        kExpectBody,
+        kGotAll,
+    };
+
+    size_t pos = 0;
+    ParseRequestState request_state = kExpectRequestLine;
+    // 解析RequestLine  GET /index.html?name=john&age=25 HTTP/1.1\r\n
+    if(kExpectRequestLine == request_state)
+    {
+        size_t request_line = data.find("\r\n", pos);
+        if(request_line == std::string::npos)
+        {
+            return false;
+        }
+
+        std::string_view strLine(data.data(), request_line);
+        // << "HttpContext::PraseRequest request_line:=" << request_line << std::endl;
+        bool bRequestLine = ProcessRequestLine(strLine);
+        if(!bRequestLine)
+        {
+            return false;
+        }
+        
+        pos = request_line + 2;
+        request_state = kExpectHeaders;
+    }
+    
+
+    // 解析后面的 KeyName
+    /*
+    Host: www.example.com\r\n
+    User-Agent: Mozilla/5.0\r\n
+    Accept: text/html\r\n
+    \r\n
+    */
+   
+    // header list 没有"\r\n\r\n" 直接返回错误，就不做解析了。
+    size_t header_end_pos = data.find("\r\n\r\n", pos);
+    if(std::string::npos == header_end_pos)
+    {
+        return false;
+    }
+
+    header_end_pos += 2;
+    while(request_state == kExpectHeaders)
+    {
+        // 找不到\r\n则出错
+        size_t eol = data.find("\r\n", pos);
+        if(std::string::npos == eol)
+        {
+            return false;
+        }
+
+        
+        std::string_view strHeader(data.data() + pos, eol - pos);
+        if(!strHeader.empty())
+        {
+           if(!ProcessHeader(strHeader))
+           {
+                return false;
+           }
+        }
+        
+        pos = eol + 2;
+
+        // pos移动到"\r\n\r\n"中的第2个\r的时候就可以结束整个header解析流程了。
+        if(pos == header_end_pos)
+        {
+            pos += 2; // 移动到header结束，开启body内容
+            request_state = kExpectBody;
+            break;
+        }
+    }
+
+    if(kExpectBody == request_state)
+    {
+        auto itr = std::find_if(headers_.begin(), headers_.end(), [](const auto& header_data){
+            return (0 == header_data.first.compare("Content-Length"));
+        });
+
+        if(itr == headers_.end())
+        {
+            request_state = kGotAll;
+            return true;
+        }
+        
+        const std::string& str_content_len = (itr->second);
+        size_t content_len = std::strtoul(str_content_len.c_str(), nullptr, 10);
+        int remain_len = data.size() - pos;
+        if(remain_len < content_len)
+        {
+            return false;
+        }
+
+        body_.assign(data.data() + pos, content_len);
+        request_state = kGotAll;
+    }
+
+    return  (kGotAll == request_state); 
+}
+
+
+bool HttpContext::ProcessRequestLine(const std::string_view& line)
+{
+    size_t method_index = line.find(' ');
+    if(method_index == std::string_view::npos){
+        return false;
+    }
+
+    size_t path_index = line.find(' ', method_index + 1);
+    if(path_index == std::string_view::npos)
+    {
+        return false;
+    }
+
+    method_ = line.substr(0, method_index);
+    path_ = line.substr(method_index + 1, path_index - method_index -1);
+    version_ = line.substr(path_index + 1);
+    return version_.find("HTTP/") == 0;
+
+}
+
+bool HttpContext::ProcessHeader(const std::string_view& line)
+{
+    size_t key_index = line.find(':');
+    if(key_index == std::string_view::npos){
+        return false;
+    }
+
+    auto trimFunc = [](std::string_view& sv){
+        size_t start_index = sv.find_first_not_of(" \t");
+        if(start_index == std::string_view::npos)  start_index = sv.size();
+
+        size_t end_index = sv.find_last_not_of(" \t");
+        if(end_index == std::string_view::npos) end_index = sv.size();
+
+        sv = sv.substr(start_index, end_index - start_index + 1);
+    };
+
+    std::string_view strKey = line.substr(0, key_index);
+    trimFunc(strKey);
+
+    std::string_view strValue = line.substr(key_index + 1);
+    trimFunc(strValue);
+
+    headers_.emplace_back(std::pair<std::string, std::string>(strKey, strValue));
+    return true;
+}
+
+/*
+std::string strCode = std::move(GetStatusCodeMsg(nStatusCode));
+    std::ostringstream response;
+    response << "HTTP/1.1 " << nStatusCode << " " << strCode.c_str() << " \r\n"
+             << "Content-Length: " << strContent.size() << "\r\n"
+             << "Content-Type: text/html\r\n"
+             << "\r\n"
+             << strContent;
+*/
+
+bool HttpContext::ParseResponse(const std::string& data)
+{
+    size_t header_end_pos = data.find("\r\n\r\n");
+    if(std::string::npos == header_end_pos)
+    {
+        return false;
+    }
+
+    size_t space = data.find(' ');
+    if(std::string::npos == space) {
+        return false;
+    }
+
+    const char* status_code_start = data.data() + space + 1;
+    while(' ' == (*status_code_start) || '\t' == (*status_code_start))
+    {
+        status_code_start += 1;
+    }
+
+    // 得到status_code,并且走完code部分
+    int tmp_status_code = std::strtoul(status_code_start, nullptr, 10);
+    // 没有必要解析这个
+    /*
+    status_code_start += 1;
+    while(' ' != (*status_code_start))
+    {
+        status_code_start += 1;
+    }
+
+    // 到达空格，继续过滤空格
+    while(' ' == (*status_code_start) || '\t' == (*status_code_start))
+    {
+        status_code_start += 1;
+    }
+
+    const char*  code_msg_start = status_code_start;
+    const char*  code_msg_end = code_msg_start;
+    int msg_char_num = 0;
+    while(' ' != (*code_msg_end))
+    {
+        msg_char_num   += 1;
+        code_msg_end   += 1;
+    }
+
+    std::string code_msg(code_msg_start, msg_char_num);
+    */
+
+    status_code_ = tmp_status_code;
+    response_body_ = std::move(data.substr(header_end_pos + 4));
+    return true;
+}
---
+++ 网络库开发-day071-HTTP增强功能-静态文件服务和sendfile零拷贝/src/LibRpc/RpcClient.h
@@ -0,0 +1,100 @@
+#pragma once 
+
+#include <memory>
+#include <atomic>
+#include <cstdint>
+#include <string>
+#include <future>
+#include <mutex>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+#include <condition_variable>
+
+
+class TcpConnection;
+class TcpClient;
+class EventLoopThread;
+class EventLoop;
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+//using TcpClientPtr = std::shared_ptr<TcpClient>;
+
+class RpcClient: public std::enable_shared_from_this<RpcClient>
+{
+public:
+    using AsyncCallback = std::function<void(const std::string& result, int32_t error)>;
+
+    explicit RpcClient(TcpConnectionPtr con);
+
+    explicit RpcClient(const std::string& strIp, int nPort);
+
+    ~RpcClient();
+
+    void SetConnection(const TcpConnectionPtr& con);
+
+    bool AutoConnect();  // 连接池自动连接 
+
+    // 自动连接的时候调用
+    const std::string& GetIp() const { return ip_; }
+
+    int GetPort() const { return port_; }
+
+    // 同步调用， 阻塞直到收到响应或者超时
+    std::string Call(const std::string& method, const std::string& params, int timeout_ms = 5000);
+
+    // 模板化call的作用
+    template<typename Req, typename Resp>
+    Resp Call(const std::string& method, const Req& req, int timeout_ms = 5000)
+    {
+        std::string params = nlohmann::json(req).dump();
+        std::string resp_str = Call(method, params, timeout_ms);
+        return nlohmann::json::parse(resp_str).get<Resp>();
+    }
+
+    // 处理响应(由网络消息回调调用)
+    void OnResponse(const std::string& data);
+
+    // 连接关闭时调用(由TcpConnection的关闭回调触发)
+    void OncConnectionClosed();
+
+    void ProcessOnResponseByAsyncCall(uint64_t res_id, int32_t code, const std::string& result, bool bTimeOut = false);
+
+public:
+    void CallAsync(const std::string& method, const std::string& params, AsyncCallback cb, int timeout_ms = 5000);
+
+    void ProcessOnResponseByCall(uint64_t res_id, int32_t code, const std::string& result);
+
+    // 连接状态查询
+    bool IsConnected() const { return bConnected_.load(std::memory_order_acquire); }
+
+    //void LoopQuit(); { if(loop_) {loop_->Quit();}  io_thread.join(); }
+
+private:
+    static std::string GenerateTraceId();
+
+private:
+    std::pair<uint64_t, std::future<std::string>> SendRequest(const std::string& method, const std::string& params);
+
+private:
+    TcpConnectionPtr con_;
+    std::shared_ptr<TcpClient>          tcp_client_;     // 用连接池自动连接时这个变量有效，手动连接时这个变量无效
+    std::unique_ptr<EventLoopThread>    loop_thread_;    // 自动连接时候持有独立的线程进行io
+    EventLoop* loop_ = nullptr;                 // 方便访问
+
+
+    std::atomic<uint64_t>   next_id_ = 1; // 奇数的id让同步Call使用
+
+    std::unordered_map<uint64_t, std::promise<std::string>> pending_;
+    std::mutex mutex_;
+
+    std::atomic<bool> bConnected_ = false; // 连接状态标志
+
+    // 测试使用
+    //std::unordered_map<uint64_t, std::chrono::time_point<std::chrono::steady_clock>> test_pending_time_;
+
+    std::atomic<uint64_t>   async_call_next_id_ = 2; // 偶数的id让异步AsyncCall使用
+    std::map<uint64_t, AsyncCallback> async_callback_pending_func_;
+    std::mutex aync_mutex_;
+
+    std::string ip_;
+    int port_;
+};
