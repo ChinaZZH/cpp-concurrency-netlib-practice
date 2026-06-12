00f1e3ca9f49e5b33365f76d7d01ada7dd8ec41e
ChinaZZH <565936130@qq.com>
2026-06-12 18:01:11 +0800

    版本初始化(迁移至上一个版本)-day075-定时器增加取消机制

---
+++ 网络库开发-day075-极致性能优化-定时器优化—增加取消定时器机制/src/Http/HttpServer.cpp
@@ -0,0 +1,309 @@
+#include "HttpServer.h"
+#include "HttpContext.h"
+#include "HttpWebService.h"
+#include "HttpRouteParamHandler.h"
+#include "../TcpConnection.h"
+#include "../EventLoop.h"
+#include "../Decoder/HttpContentDecoder.h"
+#include "../Common/ConfigManager.h"
+#include <sstream>
+#include <cassert>
+#include <functional>
+#include <nlohmann/json.hpp>
+
+HttpServer::HttpServer(EventLoop* loop, int nPort)
+:server_(loop, nPort)
+,web_service_(std::make_unique<HttpWebService>())
+,route_handler_(std::make_unique<HttpRouteParamHandler>())
+{
+    
+    // 由于httpServer 计算很少，以读写io为主，则让计算和消息都在io线程处理(不要放入任务线程池反而性能更低)。
+    server_.SetMessageCallBack(std::bind(&HttpServer::OnMessage, 
+        this, std::placeholders::_1, 
+        std::placeholders::_2)
+    );
+
+    /*
+    server_.SetMessageCallBack(std::bind(&HttpServer::AsyncOnMessage, 
+        this, std::placeholders::_1, 
+        std::placeholders::_2)
+    );
+    */
+
+    server_.SetConnectionCallBack([](const std::shared_ptr<TcpConnection>& con){
+        auto http_decoder = std::make_unique<HttpContentDecoder>();
+        con->SetDecoder(std::move(http_decoder));
+    });
+
+    web_service_->ServerStatic("/", "../www");   // 映射 URL 根路径到 ../www 目录
+}
+
+HttpServer::~HttpServer() = default;
+
+void HttpServer::Start(int option, int nEventLoopThread, int nTaskThreadNum /*= std::thread::hardware_concurrency()*/)
+{
+    server_.Start(option, nEventLoopThread, nTaskThreadNum);
+}
+
+
+
+// 异步实现 注释版本同步实现 因为解析这个过程计算过程很快，所以这个仅仅是用来学习，实际过程中应该使用同步
+void HttpServer::AsyncOnMessage(const std::shared_ptr<TcpConnection>& con, std::string& strMsg)
+{
+    ThreadPool* pool = server_.GetThreadPool();
+    if(!pool)
+    {
+        return;
+    }
+
+
+    EventLoop* loop = con->GetLoop();
+    loop->AssertInLoopThread("HttpServer::AsyncOnMessage");
+    std::weak_ptr<TcpConnection> weakConPtr = con;
+
+     // 将业务逻辑提交到线程池处理
+    pool->AddTask([this](EventLoop* loop, const std::weak_ptr<TcpConnection>& conWeakPtr, std::string strMsg){
+        //auto con = weakConPtr.lock();
+        //if(con)
+        {
+            // 模拟耗时业务（例如解析、数据库查询）
+            thread_local HttpContext async_thread_context;
+            async_thread_context.Reset();
+            bool bOk = async_thread_context.ParseRequest(strMsg);
+            if(!bOk)
+            {
+                // 解析失败或需要更多数据，这里简单返回 400
+                std::string body = "Bad Request";
+                AsyncSendHttpResponse(loop, conWeakPtr, body, 400, async_thread_context.KeepAlive());
+                return;
+            }
+
+            const std::string& strPath = async_thread_context.GetPath();
+            const std::string& strMethod = async_thread_context.GetMethod();
+            bool bKeepAlive = async_thread_context.KeepAlive();
+            std::cout << "HttpServer::AsyncOnMessage keepalive:=" << (bKeepAlive ? 1:0) << std::endl; 
+            if("POST" != strMethod)
+            {
+                // 注意安全检查，避免出目录
+                // 简单路由：返回 "Hello, World!"
+                if("/" == strPath)
+                {
+                    std::string body = "<h1>Hello, World!</h1>";
+                    AsyncSendHttpResponse(loop, conWeakPtr, body, 200, bKeepAlive);
+                }
+                else
+                {
+                    std::string filepath = std::move(web_service_->GetSystemFilePath(strPath));
+                    if('/' == filepath.back())
+                    {
+                        filepath += "index.html";
+                    }
+
+                    web_service_->AysncSendStaticFile(filepath, loop, conWeakPtr, bKeepAlive);
+                }
+
+
+                return;
+            }
+
+            
+            std::string strHandleKey(strPath.data() + 1, strPath.size()-1);  
+            auto func = this->GetHadlerByMethod(strHandleKey);
+            if(!func)
+            {
+                // 简单路由：返回 "Hello, World!"
+                std::string body = "Not Found";
+                AsyncSendHttpResponse(loop, conWeakPtr, body, 404, bKeepAlive);
+                return;
+            }
+
+            {
+                const std::string& strBody = async_thread_context.GetBody();
+                std::string result;
+                try
+                {
+                    result = func(strBody);
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << "HttpServer func error: " << e.what() << std::endl;
+                }
+                        
+                AsyncSendHttpResponse(loop, conWeakPtr, result, 200, bKeepAlive);
+            }
+        }
+        
+    }, loop, weakConPtr, std::move(strMsg));
+}
+
+void HttpServer::AsyncSendHttpResponse(EventLoop* loop, const std::weak_ptr<TcpConnection>& conWeakPtr, const std::string& strContent, int nStatusCode, bool bKeepAlive)
+{
+    if(!loop)
+    {
+        return;
+    }
+
+    auto response = HttpContext::GenerateResponseBySolveRequest(bKeepAlive, nStatusCode, strContent);
+
+
+    //std::string strResponse = response.str();
+    //std::weak_ptr<TcpConnection> weakConPtr = con;
+    
+    {
+        loop->RunInLoop([this, bKeepAlive, conWeakPtr, response = std::move(response), strTrunk = std::move(strContent)](){ 
+            //if(weakConPtr.lock()) weakConPtr.lock()->Send(strResponse);
+                std::shared_ptr<TcpConnection> con = conWeakPtr.lock(); 
+                HttpServer::SyncResponseToClient(con, response.first, response.second, strTrunk, bKeepAlive, SyncResponseToken());
+                /*
+                if(con && !con->IsWriteClosed())
+                {
+                    con->Send(response.first);
+                    if(response.second)
+                    {
+                        HttpServer::SendChunkedData(con, strContent);
+                    }
+
+                    if(!bKeepAlive)
+                    {
+                        con->CloseWhenWriteFinish();
+                    }
+                } 
+                */
+        });
+    }
+    
+}
+
+
+void HttpServer::SyncResponseToClient(const std::shared_ptr<TcpConnection>& con, const std::string& strContent, bool bChunk, const std::string& strTrunkData, bool bKeepAlive, SyncResponseToken token)
+{
+    if(con && !con->IsWriteClosed())
+    {
+        con->Send(strContent);
+        if(bChunk)
+        {
+            HttpServer::SendChunkedData(con, strTrunkData);
+        }
+
+        if(!bKeepAlive)
+        {
+            con->CloseWhenWriteFinish();
+        }
+    } 
+}
+
+
+// 同步实现版本
+void HttpServer::OnMessage(const std::shared_ptr<TcpConnection>& con, std::string& strMsg)
+{
+    http_server_context_.Reset();
+    bool bOk = http_server_context_.ParseRequest(strMsg);
+    if(!bOk)
+    {
+        // 解析失败或需要更多数据，这里简单返回 400
+        std::string body = "Bad Request";
+        SendHttpResponse(con, body, 400, http_server_context_.KeepAlive());
+        return;
+    }
+
+    const std::string& strPath = http_server_context_.GetPath();
+    const std::string& strMethod = http_server_context_.GetMethod();
+    if("POST" != strMethod)
+    {
+        HttpRouteParamHandler::RouteParams params;
+        HttpRouteParamHandler::RouteFunc func;
+        if(route_handler_->MatchRoute(strPath, params, func))
+        {
+            func(con, params, http_server_context_.KeepAlive());
+            return;
+        }
+
+
+        if(strPath == "/")
+        {
+            // 注意安全检查，避免出目录
+            // 简单路由：返回 "Hello, World!"
+            std::string body = "<h1>Hello, World!</h1>";
+            SendHttpResponse(con, body, 200, http_server_context_.KeepAlive());
+        }
+        else
+        {
+            std::string filepath = std::move(web_service_->GetSystemFilePath(strPath));
+            if('/' == filepath.back())
+            {
+                filepath += "index.html";
+            }
+
+            web_service_->SendStaticFile(filepath, con, http_server_context_.KeepAlive());
+        }
+        
+
+        return;
+    }
+
+   
+    std::string strHandleKey(strPath.data() + 1, strPath.size()-1);  
+    auto func = this->GetHadlerByMethod(strHandleKey);
+    if(func)
+    {
+        const std::string& strBody = http_server_context_.GetBody();
+        std::string result = func(strBody);
+        SendHttpResponse(con, result, 200, http_server_context_.KeepAlive());
+    }
+    else
+    {     
+        // 简单路由：返回 "Hello, World!"
+        std::string body = "Not Found";
+        SendHttpResponse(con, body, 404, http_server_context_.KeepAlive());
+    }
+}
+
+void HttpServer::SendHttpResponse(const std::shared_ptr<TcpConnection>& con, const std::string& strContent, int nStatusCode, bool bKeepAlive)
+{
+    auto response = HttpContext::GenerateResponseBySolveRequest(bKeepAlive, nStatusCode, strContent);
+    HttpServer::SyncResponseToClient(con, response.first, response.second, strContent, bKeepAlive, HttpServer::SyncResponseToken());
+}
+
+
+void HttpServer::RegisterMethod(const std::string& strMethod, Handler handler)
+{
+    method_handlers_[strMethod] = handler;
+}
+
+
+HttpServer::Handler HttpServer::GetHadlerByMethod(const std::string& strMethod)
+{
+    auto itr = method_handlers_.find(strMethod);
+    if(itr == method_handlers_.end())
+    {
+        return nullptr;
+    }
+
+    return (itr->second);
+}
+
+
+void HttpServer::SendChunkedData(const std::shared_ptr<TcpConnection>& con, const std::string& strContent)
+{
+    
+    auto& cfg = ConfigManager::getInstance();
+    int chunk_length = cfg.getInt("Http", "chunked_length", 10);
+     
+
+    int content_length = strContent.length(); 
+    for(int i = 0; i < content_length; i += chunk_length)
+    {
+        int remain_length = content_length - i;
+        int real_length = (remain_length < chunk_length)? remain_length : chunk_length;
+        std::string tmp_chunk_data = strContent.substr(i, real_length);
+
+        std::ostringstream ss;
+        ss << std::hex << real_length << "\r\n" << tmp_chunk_data << "\r\n";
+        con->Send(ss.str());
+    }
+
+    std::cout << std::endl;
+    con->Send("0\r\n\r\n");
+}
+
+
---
+++ 网络库开发-day075-极致性能优化-定时器优化—增加取消定时器机制/src/TcpConnection.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+#include <string>
+#include <queue>
+#include <chrono>
+#include "Buffer.h"
+
+class EventLoop;
+class ClientSocket;
+class HttpContext;
+class Decoder;
+
+
+class TcpConnection: public std::enable_shared_from_this<TcpConnection>
+{
+public:
+    TcpConnection(EventLoop* loop, int fd, bool tcpClient = false);
+    ~TcpConnection();
+
+    void ConnectEstablished();  // 注册到EventLoop
+    void Send(const std::string& strMessage);
+    void Shutdown();
+
+    EventLoop* GetLoop() const { return loop_; }
+    int GetFd() const { return fd_; }
+
+    using MessageCallBack = std::function<void(const std::shared_ptr<TcpConnection>&, std::string&)>;
+    using CloseCallBack = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+    using ConnectionCallBack = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+    using HighWaterMarkCallback = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+    using LowWaterMarkCallback = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+
+    void SetMessageCallBack(MessageCallBack cb) { messageCallBack_ = cb; }
+    void SetCloseCallBack(CloseCallBack cb) { closeCallBack_ = cb; }
+    void SetConnectionCallBack(ConnectionCallBack cb) { connectionCallBack_ = cb; }
+    
+    // 水位以及其相关变量
+    void SetWaterMarkCallbacks(HighWaterMarkCallback highCb, LowWaterMarkCallback lowCb, size_t highMark, size_t lowMark = 0);
+    bool IsPause() const { return pause_; }
+
+    void WaitForWaterToLowMask(const std::string& strData);
+    void WaterFromHighToLow();
+
+    void ClosedConnection() { closed = true; }
+    bool IsClosed() { return closed; }
+    bool IsWriteClosed() { return IsClosed() || closedWrite_; }
+    bool IsValid()  { return !IsWriteClosed(); }
+
+    // 空闲连接超时检测，检测到了就关闭了
+    std::chrono::steady_clock::time_point GetLastActiveTime() const { return lastActiveTime_; }
+    void UpdateLastActiveTime()  { lastActiveTime_ = std::chrono::steady_clock::now(); }
+
+    // 设置对应的解码器
+    void SetDecoder(std::unique_ptr<Decoder> decoder);
+
+    // http client 发送短连接 connection:close 的时候需要等数据全部发送完的时候进行shutdown
+    void CloseWhenWriteFinish();   // 主动关闭接口
+
+private:
+    void HandleRead();
+    void HandleWrite();
+    void HandleClose(std::string strCloseInfo);
+    void HandleError();
+
+    void SendOutput();  // 尝试发送outputBuffer_中的数据
+    void ProcessInputBuffer();
+
+    // 在Send或者SendOutput中调用内部函数
+    void CheckWaterMark();
+
+private:
+    EventLoop* loop_;
+    int fd_;
+    std::unique_ptr<ClientSocket>  socket_;
+    Buffer inputBuffer_;       // 输入缓冲区
+    Buffer outputBuffer_;      // 输出缓冲区
+    
+    MessageCallBack messageCallBack_;
+    CloseCallBack closeCallBack_;
+    ConnectionCallBack connectionCallBack_;
+    
+
+    // 发送的时候设置高低水位，outputBuffer_.size() 超过高水位的时候就没法往 outputBuffer_进行Append数据
+    // 当OutputBuffer的数据经过一系列的消化到达低水位的时候，就开启可以重新往OutputBuffer写数据
+    size_t highWaterMark_ = 0;     // 高水位，高于这个这个高水位的值就不执行Send函数，禁止向OutputBuffer进行Append
+    size_t lowWaterMark_ = 0;      // 低水位，低于这个这个低水位的值就重新开放执行Send函数的权限，开放向OutputBuffer进行Append
+
+    HighWaterMarkCallback highWaterMarkCallback_;
+    LowWaterMarkCallback  lowWaterMarkCallback_;
+    bool pause_ = false;
+    std::queue<std::string>  WaitLowWaterToSend_;
+    bool close_when_write_finish_ = false;
+    
+    bool closed = false;
+    bool closedWrite_ = false;
+
+    // 延迟删除的时候使用
+    std::chrono::steady_clock::time_point lastActiveTime_;
+
+    std::unique_ptr<Decoder> decoder_;
+};
