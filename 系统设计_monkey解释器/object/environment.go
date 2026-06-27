0ac20c32df2e3edb61e1eae6b7a97eb19cacdcd6
ChinaZZH <565936130@qq.com>
2026-06-27 15:37:43 +0800

    变量和函数调用和这两个的求值器

---
+++ 系统设计_monkey解释器/object/environment.go
@@ -0,0 +1,36 @@
+package object
+
+// Environment 表示一个作用域环境
+type Environment struct {
+	store map[string]Object
+	outer *Environment // 外层环境，用于作用域链
+}
+
+// NewEnvironment 创建一个新的顶层环境
+func NewEnvironment() *Environment {
+	tmp_store := make(map[string]Object)
+	return &Environment{store: tmp_store, outer: nil}
+}
+
+// NewEnclosedEnvironment 创建一个嵌套环境
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
+// Get 在环境中查找变量，如果不存在则在外层查找
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	if !ok && nil != e.outer {
+		return e.outer.Get(name)
+	}
+
+	return obj, ok
+}
+
+// Set 在当前环境中绑定变量
+func (e *Environment) Set(name string, value Object) Object {
+	e.store[name] = value
+	return value
+}
