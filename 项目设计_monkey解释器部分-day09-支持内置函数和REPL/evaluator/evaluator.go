e7cb8e39ef606cb348d0dc5587f103842e871b39
ChinaZZH <565936130@qq.com>
2026-06-29 09:15:04 +0800

    支持内置函数和repl

--- 项目设计_monkey解释器部分-day09-支持内置函数和REPL/evaluator/evaluator.go
+++ 项目设计_monkey解释器部分-day09-支持内置函数和REPL/evaluator/evaluator.go
@@ -7,6 +7,17 @@ import (
 )
 
 // ============================================================
+// 内置函数表
+// ============================================================
+var builtin_table = map[string]*object.Builtin{
+	"len":   &object.Builtin{Fn: builtinLen},
+	"first": &object.Builtin{Fn: builtinFirst},
+	"rest":  &object.Builtin{Fn: builtinRest},
+	"push":  &object.Builtin{Fn: builtinPush},
+	"puts":  &object.Builtin{Fn: builtinPuts},
+}
+
+// ============================================================
 // 全局变量（常用对象复用）
 // ============================================================
 var (
@@ -268,11 +279,16 @@ func evalBlockStatement(node *ast.BlockStatement, env *object.Environment) objec
 // evalIdentifier 查找并返回标识符的值
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
 	value, ok := env.Get(node.Value)
-	if !ok {
-		return NULL
+	if ok {
+		return value
 	}
 
-	return value
+	builtinObj, ok := builtin_table[node.Value]
+	if ok {
+		return builtinObj
+	}
+
+	return NULL
 }
 
 /*
@@ -315,6 +331,8 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 		}
 
 		return result
+	case *object.Builtin:
+		return fn.Fn(args...)
 	default:
 		// 非函数对象被调用，返回错误
 		return newError("not a function:%s", fn.Type())
@@ -401,4 +419,87 @@ func isError(obj object.Object) bool {
 func newError(format string, a ...interface{}) object.Object {
 	// 返回 NULL 或特殊错误对象
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func builtinLen(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments, got=%d, want=1", len(args))
+	}
+
+	switch arg := args[0].(type) {
+	case *object.String:
+		return &object.Integer{Value: int64(len(arg.Value))}
+	case *object.Array:
+		return &object.Integer{Value: int64(len(arg.Element))}
+	case *object.Hash:
+		return &object.Integer{Value: int64(len(arg.Pairs))}
+	default:
+		return newError("argument to `len` not supported, got %s", arg.Type())
+	}
+}
+
+func builtinFirst(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments, got=%d, want=1", len(args))
+	}
+
+	argType := args[0].Type()
+	if object.ARRAY_OBJ != argType {
+		return newError("argument to `first` must be Array, got=%s", argType)
+	}
+
+	arr := args[0].(*object.Array)
+	if len(arr.Element) > 0 {
+		return arr.Element[0]
+	}
+
+	return NULL
+}
+
+func builtinRest(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments, got=%d, want=1", len(args))
+	}
+
+	argType := args[0].Type()
+	if object.ARRAY_OBJ != argType {
+		return newError("argument to `first` must be Array, got=%s", argType)
+	}
+
+	array := args[0].(*object.Array)
+	length := len(array.Element)
+	if length > 0 {
+		newElement := make([]object.Object, length-1)
+		copy(newElement, array.Element[1:length])
+		return &object.Array{Element: newElement}
+	}
+
+	return NULL
+}
+
+func builtinPush(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments, got=%d, want=1", len(args))
+	}
+
+	argType := args[0].Type()
+	if object.ARRAY_OBJ != argType {
+		return newError("argument to `first` must be Array, got=%s", argType)
+	}
+
+	array := args[0].(*object.Array)
+	oldLength := len(array.Element)
+
+	newElement := make([]object.Object, oldLength+1)
+	copy(newElement, array.Element)
+	newElement[oldLength] = args[1]
+	return &object.Array{Element: newElement}
+}
+
+func builtinPuts(args ...object.Object) object.Object {
+	for _, arg := range args {
+		fmt.Println(arg.Inspect())
+	}
+
+	return NULL
 }
