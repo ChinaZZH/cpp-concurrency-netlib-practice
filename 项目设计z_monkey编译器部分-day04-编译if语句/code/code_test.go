035f6a85a0a813c177584426f9831fa972d34293
ChinaZZH <565936130@qq.com>
2026-07-01 09:58:54 +0800

    版本初始化(迁移至上一个版本)-编译if语句

---
+++ 项目设计z_monkey编译器部分-day04-编译if语句/code/code_test.go
@@ -0,0 +1,36 @@
+package code
+
+import "testing"
+
+func TestMake(t *testing.T) {
+	tests := []struct {
+		op       Opcode
+		operands []int
+		expected []byte
+	}{
+		{OpConstant, []int{65534}, []byte{byte(OpConstant), 255, 254}},
+	}
+
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+		if len(instruction) != len(tt.expected) {
+			t.Errorf("instruction length wrong: wanted=%d, got=%d", len(tt.expected), len(instruction))
+		}
+
+		for i, b := range tt.expected {
+			if b != instruction[i] {
+				t.Errorf("wrong byte at %d: want=%d, got=%d", i, b, instruction[i])
+			}
+		}
+	}
+}
+
+func TestDisassemble(t *testing.T) {
+	instructions := Make(OpConstant, 123)
+	output := Disassmble(instructions)
+
+	expected := "0000: OpConstant 123\n"
+	if output != expected {
+		t.Errorf("disassembly mismatch:\nexpected:\n%s\ngot:\n%s", expected, output)
+	}
+}
